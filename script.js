b5fbf8dda36a3ab04f89abb60fc5e3babe6246ca
Adrianus Tirta <adrianus.tirta@ymail.com>
2016-05-17 14:19:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say(`Hi there!\nI'm the personal bot of Tirta, an entrepreneur from Bandung.\nSo you want to learn about Tirta?\nJust say HELLO to get started.`)
-            bot.say(`Baris2`)
+            return bot.say(`Hi there!\n!I'm the personal bot of Tirta, an entrepreneur from Bandung.\nSo you want to learn about Tirta?\nJust say HELLO to get started.`)
                 .then(() => 'speak');
         }
     },
